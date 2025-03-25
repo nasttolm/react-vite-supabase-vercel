234710609bf023ab17de5f5a6d1db04c2e1400f6
nasttolm <nasttolm@gmail.com>
2025-03-25 08:15:56 +0000

    Fix app bar

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { Toaster } from "react-hot-toast";
 
-
 import "./index.css";
 import App from "./App.jsx";
 import AppBar from "./containers/AppBar";
 import AuthProvider from "./context/AuthContext.jsx";
-// import githubLogo from "/github.svg";
 import SignIn from "./pages/auth/SignIn.jsx";
 import SignUp from "./pages/auth/SignUp.jsx";
 import UpdatePassword from "./pages/auth/update-password"
@@ -24,7 +22,6 @@ import '@fontsource/source-serif-pro/400.css';
 import '@fontsource/source-serif-pro/600.css';
 import '@fontsource/source-serif-pro/700.css';
 import '@fontsource/source-serif-pro/900.css';
-
 
 createRoot(document.getElementById("root")).render(
   <div
@@ -43,16 +40,17 @@ createRoot(document.getElementById("root")).render(
     >
       <StrictMode>
         <AuthProvider>
-          <AppBar />
           <BrowserRouter>
+            <AppBar />
             <Routes>
               <Route index path="/" element={<App />} />
-              <Route index path="/auth/sign-in" element={<SignIn />} />
-              <Route index path="/auth/sign-up" element={<SignUp />} />
+              <Route path="/auth/sign-in" element={<SignIn />} />
+              <Route path="/auth/sign-up" element={<SignUp />} />
               <Route path="/auth/update-password" element={<UpdatePassword />} />
               <Route path="/create-recipe" element={<CreateRecipe />} />
               <Route path="/recipes/:id" element={<RecipePage />} />
-              <Route index path="/dashboard" element={<Dashboard/>}/>
+              <Route path="/recipes" element={<Dashboard />} />
+              <Route path="/dashboard" element={<Dashboard/>}/>
               <Route path="/profile/:userId?" element={<UserProfile />} />
               <Route path="/create-profile" element={<CreateProfile />} />
               <Route path="/planner" element={<SupabasePlanner />} />
